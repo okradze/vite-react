2bbb1e14c699a7e71c67c63e66c7f009a6fe4260
Mirian Okradze <okradzemirian@gmail.com>
2023-02-11 19:11:23 +0400

    test: setup vitest

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
-import { render } from "@testing-library/react";
+import { render } from '@testing-library/react'
 
-import App from "./App";
+import App from './App'
 
-test("renders app", () => {
-  render(<App />);
-});
+test('renders app', () => {
+  render(<App />)
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": "/src",
+      '@': '/src',
     },
   },
-});
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    css: false,
+  },
+})
